3893adcf2d4b2f904a21dfdf8f688ba4c5d443ff
Setenay Ronael İkiz <setenayronael@gmail.com>
2021-08-27 01:45:20 +0300

    initial commit + Unit test methods for MyArrayTests according to java documentation

---
+++ src/test/java/com/setenayronael/datastructuresandalgorithms/datastructure/MyArrayTests.java
@@ -0,0 +1,331 @@
+package com.setenayronael.datastructuresandalgorithms.datastructure;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class MyArrayTests {
+
+
+    //TODO initialize test for finalize, getClass, notify, notifyAll, wait, wait, wait, parallelStream, stream
+
+    /*
+     *
+     * Methods inherited from class java.lang.Object
+     * finalize, getClass, notify, notifyAll, wait, wait, wait
+     * Methods inherited from interface java.util.Collection
+     * parallelStream, stream
+     */
+
+    /**
+     * Methods inherited from class java.util.AbstractList
+     * Methods inherited from interface java.util.List
+     */
+    @Test
+    void testEquals(){
+        fail();
+    }
+
+    /**
+     * Methods inherited from class java.util.AbstractList
+     * Methods inherited from interface java.util.List
+     */
+    @Test
+    void testHashCode(){
+        fail();
+    }
+
+    /**
+     * Methods inherited from class java.util.AbstractCollection
+     * Methods inherited from interface java.util.List
+     */
+    @Test
+    void testContainsAll(){
+        fail();
+    }
+
+    /**
+     * Methods inherited from class java.util.AbstractCollection
+     */
+    @Test
+    void testToSting(){
+        fail();
+    }
+
+    /**
+     *  boolean	add(E e)
+     *  Appends the specified element to the end of this list.
+     */
+    @Test
+    void testAdd(){
+        fail();
+    }
+
+    /**
+     *  void add(int index, E element)
+     *  Inserts the specified element at the specified position in this list.
+     */
+    @Test
+    void testAddIndex(){
+        fail();
+    }
+
+    /**
+     * boolean	addAll(int index, Collection<? extends E> c)
+     * Inserts all of the elements in the specified collection
+     * into this list, starting at the specified position.
+     */
+    @Test
+    void testAddAll(){
+        fail();
+    }
+
+    /**
+     * void	clear()
+     * Removes all of the elements from this list.
+     */
+    @Test
+    void testClear(){
+        fail();
+    }
+
+    /**
+     * Object	clone()
+     * Returns a shallow copy of this ArrayList instance.
+     */
+    @Test
+    void testClone(){
+        fail();
+    }
+
+    /**
+     * boolean	contains(Object o)
+     * Returns true if this list contains the specified element.
+     */
+    @Test
+    void testContains(){
+        fail();
+    }
+
+    /**
+     * void	ensureCapacity(int minCapacity)
+     * Increases the capacity of this ArrayList instance, if necessary,
+     * to ensure that it can hold at least the number of elements s
+     * specified by the minimum capacity argument.
+     */
+    @Test
+    void testEnsureCapacity(){
+        fail();
+    }
+
+    /**
+     * void	forEach(Consumer<? super E> action)
+     * Performs the given action for each element of the Iterable until all elements have been processed
+     * or the action throws an exception.
+     */
+    @Test
+    void testForEach(){
+        fail();
+    }
+
+    /**
+     * E	get(int index)
+     * Returns the element at the specified position in this list.
+     */
+    @Test
+    void testGet(){
+        fail();
+    }
+
+    /**
+     * int	indexOf(Object o)
+     * Returns the index of the first occurrence of the specified element in this list, or -1 if this list does not contain the element.
+     */
+    @Test
+    void testIndexOf(){
+        fail();
+    }
+
+    /**
+     * boolean	isEmpty()
+     * Returns true if this list contains no elements.
+     */
+    @Test
+    void testIsEmpty(){
+        fail();
+    }
+
+    /**
+     * Iterator<E>	iterator()
+     * Returns an iterator over the elements in this list in proper sequence.
+     */
+    @Test
+    void testIterator(){
+        fail();
+    }
+
+    /**
+     * int	lastIndexOf(Object o)
+     * Returns the index of the last occurrence of the specified element in this list,
+     * or -1 if this list does not contain the element.
+     */
+    @Test
+    void testLastIndexOf(){
+        fail();
+    }
+
+    /**
+     * ListIterator<E>	listIterator()
+     * Returns a list iterator over the elements in this list (in proper sequence).
+     */
+    @Test
+    void testListIterator(){
+        fail();
+    }
+
+    /**
+     * ListIterator<E>	listIterator(int index)
+     * Returns a list iterator over the elements in this list (in proper sequence), starting at the specified position in the list.
+     */
+    @Test
+    void testListIteratorIndex(){
+        fail();
+    }
+
+    /**
+     * E remove(int index)
+     * Removes the element at the specified position in this list.
+     */
+    @Test
+    void testRemove(){
+        fail();
+    }
+
+    /**
+     * boolean	remove(Object o)
+     * Removes the first occurrence of the specified element from this list, if it is present.
+     */
+    @Test
+    void testRemoveObject(){
+        fail();
+    }
+
+    /**
+     * boolean	removeAll(Collection<?> c)
+     * Removes from this list all of its elements that are contained in the specified collection.
+     */
+    @Test
+    void testRemoveAll(){
+        fail();
+    }
+
+    /**
+     * boolean	removeIf(Predicate<? super E> filter)
+     * Removes all of the elements of this collection that satisfy the given predicate.
+     */
+    @Test
+    void testRemoveIf(){
+        fail();
+    }
+
+    /**
+     * protected void	removeRange(int fromIndex, int toIndex)
+     * Removes from this list all of the elements
+     * whose index is between fromIndex, inclusive, and toIndex, exclusive.
+     */
+    @Test
+    void testRemoveRange(){
+        fail();
+    }
+
+    /**
+     * void	replaceAll(UnaryOperator<E> operator)
+     * Replaces each element of this list with the result of applying the operator to that element.
+     */
+    @Test
+    void testReplaceAll(){
+        fail();
+    }
+
+    /**
+     * boolean	retainAll(Collection<?> c)
+     * Retains only the elements in this list that are contained in the specified collection.
+     */
+    @Test
+    void testRetainAll(){
+        fail();
+    }
+
+    /**
+     * E set(int index, E element)
+     * Replaces the element at the specified position in this list with the specified element.
+     */
+    @Test
+    void testSet(){
+        fail();
+    }
+
+    /**
+     * int size()
+     * Returns the number of elements in this list.
+     */
+    @Test
+    void testSize(){
+        fail();
+    }
+
+    /**
+     * void	sort(Comparator<? super E> c)
+     * Sorts this list according to the order induced by the specified Comparator.
+     */
+    @Test
+    void testSort(){
+        fail();
+    }
+
+    /**
+     * Spliterator<E>	spliterator()
+     * Creates a late-binding and fail-fast Spliterator over the elements in this list.
+     */
+    @Test
+    void testSpliterator(){
+        fail();
+    }
+
+    /**
+     * List<E>	subList(int fromIndex, int toIndex)
+     * Returns a view of the portion of this list between the specified fromIndex, inclusive, and toIndex, exclusive.
+     */
+    @Test
+    void testSubList(){
+        fail();
+    }
+
+    /**
+     * Object[]	toArray()
+     * Returns an array containing all of the elements in this list in proper sequence (from first to last element).
+     */
+    @Test
+    void testToArray(){
+        fail();
+    }
+
+    /**
+     * <T> T[]	toArray(T[] a)
+     * Returns an array containing all of the elements in this list in proper sequence
+     * (from first to last element); the runtime type of the returned array is that of the specified array.
+     */
+    @Test
+    void testToArrayContains(){
+        fail();
+    }
+
+    /**
+     * void	trimToSize()
+     * Trims the capacity of this ArrayList instance to be the list's current size.
+     */
+    @Test
+    void testTrimToSize(){
+        fail();
+    }
+}
